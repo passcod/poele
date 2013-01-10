ee96a8f71488244a35fd15b21621be0bbb7dd048
Félix Saparelli <me@passcod.net>
2013-01-11 02:06:23 +1300

    Dough is ready for use

--- main.go
+++ main.go
@@ -4,47 +4,95 @@ import (
 	"fmt"
 	"github.com/vmihailenco/redis"
 	"os"
+	"runtime"
 	"strconv"
+	"strings"
 	"time"
 )
 
-var (
-	client *redis.Client
-	max_pool int
-	cur_pool int
-)
+var client *redis.Client
 
 func main() {
+	quit := make(chan int, 1)
+	go handleSignals(quit)
+
 	redisurl := os.Getenv("REDIS_HOST")
 	redispass := os.Getenv("REDIS_PASSWORD")
-	
-	poele_max, err := strconv.ParseInt(os.Getenv("POELE_MAX"), 10, 0)
-	if err != nil { panic(err) }
-	max_pool = int(poele_max)
-	cur_pool = 0
-	
+
+	ncpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(ncpu)
+
 	client = redis.NewTCPClient(redisurl, redispass, -1)
 	defer client.Close()
-	defer fmt.Println("Exiting...")
-	
-	fmt.Println("Waiting on queue...")
-	for {
-		if cur_pool < max_pool {
-			jobReq := client.BRPopLPush("box.processing", "box.todo", 0)
-			if err := jobReq.Err(); err != nil {
-				fmt.Printf("!! Redis: %s\n", err)
-				continue
-			}
-			
-			cur_pool += 1
-			fmt.Printf("[%i/%i] Processing job: %s\n",
-			  cur_pool, max_pool, jobReq.Val())
-			go process(jobReq.Val())
-		}
+
+	queue := make(chan string, ncpu)
+	for i := 0; i < ncpu; i++ {
+		go doWork(i, queue)
+	}
+
+	go giveWork(queue)
+
+	<-quit
+	for i := 0; i < ncpu; i++ {
+		queue <- ""
 	}
 }
 
-func process(jobId string) {
-  time.Sleep(1)
-	cur_pool -= 1
+func giveWork(queue chan string) {
+	from := "box.todo"
+	to := "box.processing"
+
+	for {
+		item := client.BRPopLPush(from, to, 0)
+		if err := item.Err(); err != nil {
+			fmt.Printf("!!! Redis: %s\n", err)
+			continue
+		}
+
+		val := item.Val()
+		fmt.Printf(">>> Crèpes au bacon, order %s, coming up!\n", val)
+		queue <- val
+	}
+}
+
+func doWork(id int, queue chan string) {
+	fmt.Printf(">>> Firing up gas Nº%d and getting a clean pan…\n", id)
+
+	for {
+		item := <-queue
+		if item == "" {
+			break
+		}
+
+		fmt.Printf("[%d] Cooking: %s\n", id, item)
+		then := time.Now()
+
+		ret := process(item)
+
+		dur1 := uDuration(time.Since(then))
+
+		client.LPush("box.done", item)
+		client.LRem("box.processing", -1, item)
+
+		dur2 := uDuration(time.Since(then))
+
+		fmt.Printf("[%d] Done in (%s/%s): %v\n", id, dur1, dur2, ret)
+	}
+}
+
+func uDuration(d time.Duration) string {
+	return strings.Replace(d.String(), "u", "µ", 1)
+}
+
+func process(item string) int64 {
+	n, _ := strconv.ParseInt(item, 10, 64)
+	return fib(n)
+}
+
+func fib(n int64) int64 {
+	if n < 2 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
 }
